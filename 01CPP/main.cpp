65a978ce627b0dce3d09efe9cf33942600149282
dry86 <1061239880@qq.com>
2022-09-28 13:29:46 +0800

    Initial Commit

---
+++ 01CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  01CPP
+//
+//  Created by dry on 2022/9/28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
